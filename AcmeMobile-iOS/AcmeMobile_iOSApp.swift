85126f385633faf60b3458ebcc1fc3dd10211020
Deyan <drs600@inlumine.ual.es>
2023-04-05 13:10:45 +0200

    Initial Commit

---
+++ AcmeMobile-iOS/AcmeMobile_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcmeMobile_iOSApp.swift
+//  AcmeMobile-iOS
+//
+//  Created by Deyan on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct AcmeMobile_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
